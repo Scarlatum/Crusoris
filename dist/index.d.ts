27bcc2b3cb49b10a58c82e1528c5be057498c2aa
Alexey Revo <Revo515@yandex.ru>
2021-09-06 20:01:41 +0800

    update

---
+++ dist/index.d.ts
@@ -0,0 +1,12 @@
+export default class Cursor {
+    #private;
+    private domRoot;
+    private customCursor;
+    private dot;
+    private cssProperties;
+    private cursorTransform;
+    private dotTransform;
+    constructor(target: HTMLElement);
+    init(): void;
+}
+//# sourceMappingURL=index.d.ts.map
